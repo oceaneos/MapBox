f22523e04aa2df0ccf322388ba4f56fe1f0b686d
Rob | Labs <roblabs@users.noreply.github.com>
2016-08-31 21:38:28 -0700

    Added NODATA parameter for keeping transparency

--- scripts/clut.sh
+++ scripts/clut.sh
@@ -8,7 +8,7 @@ gdal_translate clut.blue2red.vrt MY1DMM_CHLORA_2016-07-01_rgb_3600x1800.clut.blu
 
 # check the red2blue color table actually took by inspecting the VRT
 gdal_translate -of VRT MY1DMM_CHLORA_2016-07-01_rgb_3600x1800.clut.blue2red.TIFF clut.blue2red.check.vrt
-gdal_translate -of vrt -expand rgba MY1DMM_CHLORA_2016-07-01_rgb_3600x1800.clut.blue2red.TIFF temp.vrt
-rm -rf temp/
+gdal_translate -of vrt -expand rgba -a_nodata 0 MY1DMM_CHLORA_2016-07-01_rgb_3600x1800.clut.blue2red.TIFF temp.vrt
+rm -rf temp/;  rm red2blue-clut-vert.mbtiles
 gdal2tilesp.py -z 0-6 temp.vrt
 mb-util temp red2blue-clut-vert.mbtiles
